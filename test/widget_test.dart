e7657ee0d047d48d35506463e90f99cf465aaac8
Catelt <54297602+Catelt@users.noreply.github.com>
2023-03-07 22:07:52 +0700

    add simple test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,5 +4,12 @@
 // utility in the flutter_test package. For example, you can send tap and scroll
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
+import 'package:flutter_test/flutter_test.dart';
+import 'package:our_tutor/app/app.dart';
 
-void main() {}
+void main() {
+  testWidgets('My simple app', (tester) async {
+    // Create the widget by telling the tester to build it.
+    await tester.pumpWidget(const App());
+  });
+}
